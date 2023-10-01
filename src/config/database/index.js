9d1094fa204a144864608140716d3a5da248008c
thanhnhuong <thanhnhuong0702@gmail.com>
2023-10-01 09:33:33 +0700

    Fix Connect database
    Kiểm tra lại xem đã kết nối database chưa. Tránh trùng lặp connect database

--- src/config/database/index.js
+++ src/config/database/index.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/Nextjs-forum-NLCS');
-        console.log('Database Connect successfully!!!');
+        if(!mongoose.connection.readyState) {
+            await mongoose.connect('mongodb://127.0.0.1:27017/Nextjs-forum-NLCS');
+            console.log('Database Connect successfully!!!');
+        }
     } catch (error) {
         console.log(error);
     }
